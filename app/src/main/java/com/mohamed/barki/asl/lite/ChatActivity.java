0138f609b763a950983d513010f4e50ef9a6618b
mohamedbarkigithub <mohamedbarkimaths@gmail.com>
2025-03-21 20:59:51 +0100

    2025/03/21 21:00

--- app/src/main/java/com/mohamed/barki/asl/lite/ChatActivity.java
+++ app/src/main/java/com/mohamed/barki/asl/lite/ChatActivity.java
@@ -12,7 +12,6 @@ import android.text.Html;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
